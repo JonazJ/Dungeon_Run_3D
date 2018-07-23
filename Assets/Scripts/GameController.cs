4d328bcbe15573ea7d3c98a40a0e7b2ef739a565
Jonas Johansson <jojo0086@student.hv.se>
2018-07-23 12:01:25 +0200

    Added Main menybase
    Main meny in the making.
    Canvas, panels and buttons ready to go.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -41,20 +41,25 @@ public class GameController : MonoBehaviour {
         gameEnded = true;
         if ( status == 5 )
         {
-            endText.text = "Gratz, you've finished the game";
+            endText.text = "Gratz, you've finished the game.";
             endText.gameObject.SetActive(true);
         }
-        else if ( status == 7 )
+        else if ( status == 6 )
         {
             endText.text = "So, you're doing everything...";
         }
-        else if (status ==  10)
+        else if (status ==  8)
         {
             endText.text = "Stop playing already!";
         }
         else if (status == 16)
         {
-            endText.text = "Wow, no fooling you. This time you actually finished the game! Impressive intuition.";
+            endText.text = "Okey game over, nothing more to see!";
+        }
+        else if (status == 16)
+        {
+            endText.text = "This time you actually finished the game. Impressive! \nTo play again Press R.";
+
         }
         else
         {
